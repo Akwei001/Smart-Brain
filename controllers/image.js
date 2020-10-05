ff0a495eba52f2189d37d5a229e4810730129d30
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2020-10-05 15:34:10 +0100

    Add files via upload

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+ const Clarifai = require('clarifai');
+
+ const app = new Clarifai.App({
+ apiKey: 'eee44278acb0493e8f6ceb1d183d41ee'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    // This part has been updated with the recent Clarifai changed. Used to be:
+    // .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+ const handleImage = (req, res, db) => {
+	const {id} = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+} 
+
+module.exports = {
+   handleImage,
+   handleApiCall
+}
